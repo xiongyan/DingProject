b9cb42fa568fa7ac9502ccdf2eda8dedae0027ed
xiong <xiong@xiongdeMacBook-Pro.local>
2018-07-25 11:53:47 +0800

    add category
    Signed-off-by: xiong <xiong@xiongdeMacBook-Pro.local>

---
+++ src/main/java/com/project/service/CategoryService.java
@@ -0,0 +1,12 @@
+package com.project.service;
+
+import com.project.dao.Result;
+
+public interface CategoryService {
+
+    /**
+     * 获取 分类列表
+     * @return
+     */
+    Result getCategories();
+}
